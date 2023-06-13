8ccff65f7dbcb64d4e7fb0a7b122160867fd8a89
segonz <segonz@bancolombia.com.co>
2023-06-13 07:27:08 -0500

    se elimina todo de persona, prueba

--- src/main/java/co/com/poli/eventospoli/EventoApplication.java
+++ src/main/java/co/com/poli/eventospoli/EventoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonaApplication {
+public class EventoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonaApplication.class, args);
+		SpringApplication.run(EventoApplication.class, args);
 	}
 
 }
--- src/main/java/co/com/poli/eventospoli/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package co.com.poli.eventospoli.repository;
-
-import co.com.poli.eventospoli.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
